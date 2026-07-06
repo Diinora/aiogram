19d532d4eac5c6651b5e899b090c1bda52713c70
Diinora <dinoram1711@gmail.com>
2026-07-06 16:31:33 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart,Command
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
+
+from dotenv import load_dotenv
+import os
+load_dotenv()
+Token = os.getenv("API")
+bot = Bot(token=Token)
+dp = Dispatcher()
+
+menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text='Katalog'), KeyboardButton(text='Sebet')],
+        [KeyboardButton(text='Baylanis')],
+
+    ],
+    resize_keyboard=True
+)
+
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer("Assalawma aleykum 😊",reply_markup=menu)
+
+
+
+
+async def main():
+    print('bot iske qosildi')
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
